13b6c9549c720990b0720044249eb1853e589310
Lanzoor <166265896+Lanzoor@users.noreply.github.com>
2024-11-13 10:43:18 +0900

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,14 @@
 import platform, time, tkinter, random, json, os, webbrowser
 
-def animated_print(message: str) -> None:
+def animated_print(message: str, delay: int = 0.005, end: bool = True) -> None:
     for char in message:
         print(char, end = "", flush = True)
-        time.sleep(0.05)
+        time.sleep(delay)
+    if end:
+        print("")
 
-def animated_input(message: str):
-    animated_print(message)
+def animated_input(message: str, delay: int = 0.005):
+    animated_print(message, delay, False)
     return input("")
 
 animated_print("Starting inspection...")
@@ -22,7 +24,7 @@ windows_version = int(platform.uname().release)
 
 if windows_version < 11:
     animated_print(f"""Although you can proceed just fine, you're running this program on Windows {windows_version}.
-This program uses the colorama module to implement vibrant colors, but terminals in Windows 10 or lower won't support those coloring. 
+This program uses the colorama module to implement vibrant colors, but terminals in Windows 10 or less won't support those coloring. 
 It's highly recommended to run this program in a VSCode terminal if you want to proceed.""")
 
 animated_print(f"Recommended Windows version passed! (Recommended: Windows 11~, Current: {windows_version})")
@@ -64,12 +66,7 @@ multiplier = savedata['multiplier']
 command_count = savedata['commandCount']
 shop_upgrades = savedata['shopUpgrades']
 settings = savedata['settings']
-
-def animated_print(message: str) -> None:
-    global settings
-    for char in message:
-        print(char, end = "", flush = True)
-        time.sleep(0.003 if settings['useAnimation'] else 0)
+delay = 0.005 if settings['useAnimation'] else 0
 
 def save_all_data():
     global savedata, username, points, multiplier, command_count, shop_upgrades, settings, delay
@@ -79,27 +76,28 @@ def save_all_data():
     savedata['commandCount'] = command_count
     savedata['shopUpgrades'] = shop_upgrades
     savedata['settings'] = settings
+    delay = 0.005 if settings['useAnimation'] else 0
 
     with open(savefile_path, "w") as saveddata:
         json.dump(savedata, saveddata)
     del saveddata
 
 if username == "ERR_NOT_SPECIFIED":
-    animated_print("Your username was not found, what can this program call you?")
+    animated_print("Your username was not found, what can this program call you?", delay)
     while True:
-        new_username = animated_input("Enter your new username!\n> ")
+        new_username = animated_input("Enter your new username!\n> ", delay)
         if new_username.lower() in ["null", "undefined", "None", "ERR_NOT_SPECIFIED"]:
-            animated_print(f"Sorry, but usernames can't be {colorama.Fore.BLUE + new_username + colorama.Fore.RESET}. Try a different one.")
+            animated_print(f"Sorry, but usernames can't be {colorama.Fore.BLUE + new_username + colorama.Fore.RESET}. Try a different one.", delay)
         else:
-            animated_print(f"Your brand new username is set to {colorama.Fore.BLUE + new_username + colorama.Fore.RESET}. If you want it changed, you can always change your username via ?settings.")
+            animated_print(f"Your brand new username is set to {colorama.Fore.BLUE + new_username + colorama.Fore.RESET}. If you want it changed, you can always change your username via ?settings.", delay)
             username = new_username
             savedata['username'] = new_username
             break
     save_all_data()
 
-animated_print("Almost done! Applying settings...")
+animated_print("Almost done! Applying settings...", delay)
 
-lcp_version = "v0.0.3.8-alpha"
+lcp_version = "v0.0.4-alpha"
 
 welcome_message = f"Welcome to Lanzoor Command Panel ({colorama.Fore.GREEN + lcp_version + colorama.Fore.RESET}), {colorama.Fore.BLUE + username + colorama.Fore.RESET}! Type ?help to get help about the commands you can use, or type ?exit to exit the program. Have fun!"
 
@@ -114,7 +112,6 @@ help_message = f"""Here are all commands you can use in Lanzoor Command Panel as
 ?datetime / ?timedate: Displays your date and time.
 ?rps / ?rockpaperscissors: Play the rock paper scissors game.
 ?golt / ?greaterorlowerthan: Play the greater or lower than game.
-?readme: Opens the README.md file for this project.
 ?exit: Exit Lanzoor Command Panel.
 
 And, here are some few things that you should probably consider using Lanzoor Command Panel.
@@ -125,19 +122,19 @@ If you want to enjoy the true game, I definitely recommend playing the game with
 If you want to ask / suggest / compliment about anything in Lanzoor Command Panel, feel free to ask me in Discord! (lanzoor)
 """
 
-animated_print(welcome_message)
+animated_print(welcome_message, delay)
 
 while True:
-    user_input = animated_input("> ").strip().lower()
+    user_input = animated_input("> ", delay).strip().lower()
     match user_input:
         case "?help":
-            animated_print(help_message)
+            animated_print(help_message, delay)
         case "?stats" | "?stat":
             animated_print(f"""Here are your stats on Lanzoor Command Panel;
 Your username is {username}.
 You have used {command_count} commands (excluding this command).
 You have {points} points, and your point multiplier is {multiplier}.
-You have purchased {shop_upgrades['shopUpgradesPurchased']} shop upgrades.""")
+You have purchased {shop_upgrades['shopUpgradesPurchased']} shop upgrades.""", delay)
         case "?settings":
             settings_message = f"""Welcome to the settings page, you can tweak some settings here!
 You will have to input the setting number. Choose a setting to change, and input exit or ?exit to exit settings.
@@ -150,23 +147,23 @@ In that case though, you should not try to force-quit the program (using ^C / Ct
 Savedata will be autosaved when you exit this program, though.{colorama.Style.RESET_ALL}
 3. Change Username
 {colorama.Fore.RED}4. RESET DATA{colorama.Fore.RESET}"""
-            animated_print(settings_message)
+            animated_print(settings_message, delay)
             while True:
-                settings_input = animated_input("> ").strip()
+                settings_input = animated_input("> ", delay).strip()
                 match settings_input:
                     case "1":
                         settings['useAnimation'] = not (settings['useAnimation'])
-                        animated_print(f"Set the setting \"Use Animations\" to {colorama.Fore.GREEN if settings['useAnimation'] else colorama.Fore.RED}{settings['useAnimation']}{colorama.Fore.RESET}")
+                        animated_print(f"Set the setting \"Use Animations\" to {colorama.Fore.GREEN if settings['useAnimation'] else colorama.Fore.RED}{settings['useAnimation']}{colorama.Fore.RESET}", delay)
                     case "2":
                         settings['autoSaveEveryCommand'] = not (settings['autoSaveEveryCommand'])
-                        animated_print(f"Set the setting \"Autosave Every Command\" to {colorama.Fore.GREEN if settings['autoSaveEveryCommand'] else colorama.Fore.RED}{settings['autoSaveEveryCommand']}{colorama.Fore.RESET}")
+                        animated_print(f"Set the setting \"Autosave Every Command\" to {colorama.Fore.GREEN if settings['autoSaveEveryCommand'] else colorama.Fore.RED}{settings['autoSaveEveryCommand']}{colorama.Fore.RESET}", delay)
                     case "3":
                         while True:
-                            new_username = animated_input("Enter your new username!\n> ")
+                            new_username = animated_input("Enter your new username!\n> ", delay)
                             if new_username.lower() in ["null", "undefined", "None", "ERR_NOT_SPECIFIED"]:
-                                animated_print(f"Sorry, but usernames can't be {colorama.Fore.BLUE + new_username + colorama.Fore.RESET}. Try a different one.")
+                                animated_print(f"Sorry, but usernames can't be {colorama.Fore.BLUE + new_username + colorama.Fore.RESET}. Try a different one.", delay)
                             else:
-                                animated_print(f"Your username is now set to {colorama.Fore.BLUE + new_username + colorama.Fore.RESET}.")
+                                animated_print(f"Your username is now set to {colorama.Fore.BLUE + new_username + colorama.Fore.RESET}.", delay)
                                 username = new_username
                                 break
                     case "4":
@@ -174,8 +171,8 @@ Savedata will be autosaved when you exit this program, though.{colorama.Style.RE
 You can still continue on this savedata if you create backups of the savedata.json file before this action.
 Once you perform this action, your savedata will be resetted without furthermore confirmations, and this program will automatically be closed.
 Are you sure that you want to delete your savedata? Input y to continue.{colorama.Fore.RESET}"""
-                        animated_print(confirmation_message)
-                        confirmation_input = animated_input("> ").strip().lower()
+                        animated_print(confirmation_message, delay)
+                        confirmation_input = animated_input("> ", delay).strip().lower()
                         if confirmation_input == "y":
                             
                             savedata['username'] = "ERR_NOT_SPECIFIED"
@@ -189,69 +186,68 @@ Are you sure that you want to delete your savedata? Input y to continue.{coloram
                                 json.dump(savedata, saveddata)
                             del saveddata
                             
-                            animated_print("Savedata deletion successful. This program will automatically close after a few seconds.")
+                            animated_print("Savedata deletion successful. This program will automatically close after a few seconds.", delay)
                             time.sleep(3)
                             raise Exception("Savedata reset performed; exiting the program.")
                     case "?exit" | "exit":
-                        animated_print("Exited settings.")
+                        animated_print("Exited settings.", delay)
                         break
                     case _:
-                        command_count += 1
                         continue
                 save_all_data()
                 command_count += 1
         case "?rand" | "?random" | "?roll":
             while True:
-                minimum_number = animated_input("Enter the minimum number.\n> ")
+                minimum_number = animated_input("Enter the minimum number.\n> ", delay)
                 try:
                     minimum_number = int(minimum_number)
                 except ValueError:
-                    animated_print("Please enter a valid integer.")
+                    animated_print("Please enter a valid integer.", delay)
                     continue
-                maximum_number = animated_input("Enter the maximum number.\n> ")
+                maximum_number = animated_input("Enter the maximum number.\n> ", delay)
                 try:
                     maximum_number = int(maximum_number)
                 except ValueError:
-                    animated_print("Please enter a valid integer.")
+                    animated_print("Please enter a valid integer.", delay)
                     continue
                 if minimum_number > maximum_number:
-                    animated_print(colorama.Style.DIM + "You silly! The minimum number can't be larger can the maximum number." + colorama.Style.RESET_ALL)
+                    animated_print(colorama.Style.DIM + "You silly! The minimum number can't be larger can the maximum number." + colorama.Style.RESET_ALL, delay)
                     continue
                 if minimum_number == maximum_number:
-                    animated_print(colorama.Style.DIM + "You silly! Both numbers can't be the same, otherwise it would always return the number and breaks the entire concept of random!" + colorama.Style.RESET_ALL)
+                    animated_print(colorama.Style.DIM + "You silly! Both numbers can't be the same, otherwise it would always return the number and breaks the entire concept of random!" + colorama.Style.RESET_ALL, delay)
                     continue
                 break
-            animated_print(f"You picked {colorama.Style.BRIGHT}{minimum_number}{colorama.Style.RESET_ALL} as your minimum number, and {colorama.Style.BRIGHT}{maximum_number}{colorama.Style.RESET_ALL} as your maxmimum number. Your result is {colorama.Style.BRIGHT}{random.randint(minimum_number, maximum_number)}{colorama.Style.RESET_ALL}.")
+            animated_print(f"You picked {colorama.Style.BRIGHT}{minimum_number}{colorama.Style.RESET_ALL} as your minimum number, and {colorama.Style.BRIGHT}{maximum_number}{colorama.Style.RESET_ALL} as your maxmimum number. Your result is {colorama.Style.BRIGHT}{random.randint(minimum_number, maximum_number)}{colorama.Style.RESET_ALL}.", delay)
         case "?date":
-            animated_print(f"Your current date is {colorama.Style.BRIGHT + time.strftime(r"%B %d (%A), %Y") + colorama.Style.RESET_ALL}.")
+            animated_print(f"Your current date is {colorama.Style.BRIGHT + time.strftime(r"%B %d (%A), %Y") + colorama.Style.RESET_ALL}.", delay)
         case "?time":
-            animated_print(f"Your current time is {colorama.Style.BRIGHT + time.strftime(r"%X") + colorama.Style.RESET_ALL}.")
+            animated_print(f"Your current time is {colorama.Style.BRIGHT + time.strftime(r"%X") + colorama.Style.RESET_ALL}.", delay)
         case "?datetime" | "?timedate":
-            animated_print(f"Your current date & time is {colorama.Style.BRIGHT + time.strftime(r"%B %d (%A), %Y | %X") + colorama.Style.RESET_ALL}.")
+            animated_print(f"Your current date & time is {colorama.Style.BRIGHT + time.strftime(r"%B %d (%A), %Y | %X") + colorama.Style.RESET_ALL}.", delay)
         case "?rps" | "?rockpaperscissors":
-            user_choice = animated_input("Choose one! Rock, Paper, or Scissors! You can also only input the first character of your choice.\n> ").capitalize().replace(" ","")
+            user_choice = animated_input("Choose one! Rock, Paper, or Scissors! You can also only input the first character of your choice.\n> ", delay).strip().lower()
             match user_choice:
-                case "R":
-                    user_choice = "Rock"
-                case "P":
-                    user_choice = "Paper"
-                case "S":
-                    user_choice = "Scissors"
+                case "r":
+                    user_choice = "rock"
+                case "p":
+                    user_choice = "paper"
+                case "s":
+                    user_choice = "scissors"
             
-            valid_rps_choice = ["Rock", "Paper", "Scissors"]
+            valid_rps_choice = ["rock", "paper", "scissors"]
             computer_choice = random.choice(valid_rps_choice)
             
             while user_choice not in valid_rps_choice:
-                user_choice = animated_input("You dumdum, that is not a valid choice! Try again.\n> ").capitalize().replace(" ","")
+                user_choice = animated_input("You dumdum, that is not a valid choice! Try again.\n> ", delay).capitalize().replace(" ","")
             
             if computer_choice == user_choice:
                 win_state = "It's a draw!"
                 pending_points = 2
             else:
                 winning_conditions = (
-                    ("Rock", "Scissors"),
-                    ("Scissors", "Paper"),
-                    ("Paper", "Rock"),
+                    ("rock", "scissors"),
+                    ("scissors", "paper"),
+                    ("paper", "rock"),
                 )
                 if (user_choice, computer_choice) in winning_conditions:
                     win_state = "You won!"
@@ -260,36 +256,36 @@ Are you sure that you want to delete your savedata? Input y to continue.{coloram
                     win_state = "I won!"
                     pending_points = 1
             points += pending_points * multiplier
-            animated_print(f"You picked {colorama.Style.BRIGHT + user_choice + colorama.Style.RESET_ALL}, I pick {colorama.Style.BRIGHT + computer_choice + colorama.Style.RESET_ALL}! {colorama.Style.BRIGHT + win_state + colorama.Style.RESET_ALL}\nYou gained {pending_points * multiplier} points in this game.")
+            animated_print(f"You picked {colorama.Style.BRIGHT + user_choice + colorama.Style.RESET_ALL}, I pick {colorama.Style.BRIGHT + computer_choice + colorama.Style.RESET_ALL}! {colorama.Style.BRIGHT + win_state + colorama.Style.RESET_ALL}\nYou gained {pending_points * multiplier} points in this game.", delay)
         case "?golt" | "?greaterorlowerthan":
             choice = random.randint(1, 100)
             attempts = 0
             while True:
-                user_choice = animated_input("Enter your guess!\n> ")
+                user_choice = animated_input("Enter your guess!\n> ", delay).strip()
                 try:
                     user_choice = int(user_choice)
                 except ValueError:
-                    animated_print("Please enter a valid integer.")
+                    animated_print("Please enter a valid integer.", delay)
                     continue
                 if choice < user_choice and user_choice <= 100:
-                    animated_print("My number is lower than your choice, try again!")
+                    animated_print("My number is lower than your choice, try again!", delay)
                 elif user_choice > 100:
-                    animated_print("Hey, that was out of the range!")
+                    animated_print("Hey, that was out of the range!", delay)
                 elif choice > user_choice:
-                    animated_print("My number is greater than your choice, try again!")
+                    animated_print("My number is greater than your choice, try again!", delay)
                 elif choice == user_choice:
                     attempts += 1
-                    animated_print(f"You've guessed it right! You used {attempts} attempts in this game, and the answer was {colorama.Style.BRIGHT}{choice}{colorama.Style.RESET_ALL}.")
+                    animated_print(f"You've guessed it right! You used {attempts} attempts in this game, and the answer was {colorama.Style.BRIGHT}{choice}{colorama.Style.RESET_ALL}.", delay)
                     break
                 attempts += 1
             pending_points = (10 - attempts) * multiplier
             points += pending_points
             if pending_points <= 0:
-                animated_print(f"You used too many attempts in this game! You didn't gain any points.")
+                animated_print(f"You used too many attempts in this game! You didn't gain any points.", delay)
             else:
-                animated_print(f"You gained {pending_points} points in this game.")
+                animated_print(f"You gained {pending_points} points in this game.", delay)
         case "?readme":
-            animated_print("Beaming information to your web browser...")
+            animated_print("Beaming information to your web browser...", delay)
             webbrowser.open_new("https://github.com/Lanzoor/LCP/blob/main/savedata.json")
         case "?exit":
             break
@@ -301,6 +297,6 @@ Are you sure that you want to delete your savedata? Input y to continue.{coloram
 
 save_all_data()
 
-animated_print(f"Goodbye {colorama.Fore.BLUE + username + colorama.Fore.RESET}, see you again!")
+animated_print(f"Goodbye {colorama.Fore.BLUE + username + colorama.Fore.RESET}, see you again!", delay)
 
 time.sleep(1)
